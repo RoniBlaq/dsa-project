aeae88fdea899a4273b59eaa5275f06787c43ff5
RoniBlaq <adeotiaderonke565@gmail.com>
2025-07-16 00:49:41 +0100

    fixed import issues

--- src/app/menteepage/search-mentor/page.js
+++ src/app/menteepage/search-mentor/page.js
@@ -5,31 +5,32 @@ import Link from 'next/link';
 import {useRouter} from 'next/navigation';
 
 function SearchPage() {
-    const [mentors, setMentors] = useState([....]);
+    const [mentors, setMentors] = useState([]);
     const [filteredMentors, setFilteredMentors] = useState([]);
+
     const handleSearch = (searchTerm) => {
         const filtered = mentors.filter((mentor) => mentor.name.toLowerCase().includes(searchTerm.toLowerCase()));
         setFilteredMentors(filtered);
     };
-    useEffect(() =>{
-        axiox.get()
+    useEffect(() => {
+        axiox.get('http://localhost/nextphp/mentor.php')
         .then((response) => {
             setMentors(response.data);
         })
         .catch((error) => {
-
-        })
-    })
+          console.error(error);
+        });
+    }, []);
     return(
-<div> 
-    <SearchBar onSearch={handleSearch} />
-    <div className="mentor-list">
+   <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12 xl:p-24"> 
+    <SearchBar onSearch={handleSearch} className="mb-6" />
+    <div className="mentor-list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {filteredMentors.map((mentor) => (
-            <div key={mentor.id} className="mentor-profile">
-                <h2>{mentor.name}</h2>
-                <p> {mentor.expertise}</p>
-                <p> {mentor.bio}</p>
-                <button> View Profile</button>
+            <div key={mentor.id} className="mentor-profile bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow duration-300">
+                <h2 className="text-lg font-bold mb-2">{mentor.name}</h2>
+                <p className="text-gray-600 mb-2"> {mentor.expertise}</p>
+                <p lassName="text-gray-600 mb-2"> {mentor.bio}</p>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> View Profile</button>
                  </div>
         ))}
     </div>
